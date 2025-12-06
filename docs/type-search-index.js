19a30e010778dcf91e849bcacdf180a8f0f810bb
SmallGanzo <egorov2006g@gmail.com>
2025-12-07 01:38:26 +0300

    Add full Javadoc HTML files

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"server","l":"ClientHandler"},{"p":"client","l":"ClientMain"},{"p":"server","l":"DataBase"},{"p":"client","l":"MessageRead"},{"p":"client","l":"MessageSend"},{"p":"server","l":"ServerMain"},{"p":"server","l":"Users"}];updateSearchResults();
